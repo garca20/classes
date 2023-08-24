eb173d5e2b5e81d44d42478d9b598ad7552cc77a
García Andrés <142649427+garca20@users.noreply.github.com>
2023-08-24 13:42:13 -0500

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,47 @@
+package javaapplication1;
+// import java.util.Scanner;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        /*  
+        Scanner age = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        String ageN = age.next();
+        System.out.println("You are " + ageN + " years old.");
+        System.out.println("Hola, Mundo"); // Imprime y salta una linea
+        System.out.print("Este es"); // Imprime sin salto de linea
+        System.out.print(" un mensaje"); // Continúa imprimiendo en la misma linea
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("First number:");
+        int num1 = scanner.nextInt();
+        
+        System.out.println("Second number:");
+        int num2 = scanner.nextInt();
+        int resultado = num1+num2;
+        System.out.println("la suma es: " + resultado);
+        scanner.close();
+        
+        
+        float circleArea;
+        float radius = 3;
+        final float PI = 3.1416f;
+        circleArea = radius*radius*PI;
+        System.out.println(circleArea + " cm.");
+        break;
+        
+        
+        float gradosCelsius = 100.5f;
+        float gradosFarenheit;
+        gradosFarenheit = (gradosCelsius*9/5)+32;
+        System.out.println(gradosCelsius + "°C equivalen a " + gradosFarenheit + "°F");
+
+        */
+        
+        char letra = 'A';
+        String nombre = "Juan";
+        System.out.println(nombre + " " + letra);
+    }
+} 
